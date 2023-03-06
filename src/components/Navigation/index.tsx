f82ae2f63f41ab8298d348c5e983baa26c63c3cb
MeowMeowKit <92076631+MeowMeowKit@users.noreply.github.com>
2023-03-07 01:36:02 +0700

    fix deny/accept booking

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -118,10 +118,10 @@ export const Navigation: React.FunctionComponent<NavigationProps> = () => {
 									<Menu.Item>
 										{({ active }) => (
 											<a
-												href="#"
 												className={`${
 													active ? 'bg-gray-100' : ''
 												} block px-4 py-2 text-sm text-gray-700`}
+												onClick={handleLogout}
 											>
 												Sign out
 											</a>
